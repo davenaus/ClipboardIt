e844f136b6779ab443bfd46a0713d02b0513f070
Austin Davenport <austindavenport000@gmail.com>
2025-07-30 07:26:24 -0500

    guarantee

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -435,7 +435,7 @@ const HomePage: React.FC = () => {
                   <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M9 16.17L4.83 12L3.41 13.41L9 19L21 7L19.59 5.59L9 16.17Z" fill="currentColor"/>
                   </svg>
-                  <span>30-day money-back guarantee</span>
+                  <span>Securely buy and download through Adobe Cloud</span>
                 </div>
               </div>
             </div>
